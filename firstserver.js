b3a3485aad6fca49cd047370f535d0ad5e260e5d
Hong Sung <profsung@gmail.com>
2018-01-22 20:44:35 -0600

    added

--- firstserver.js
+++ firstserver.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const s = http.createServer((req, res) => {
-    const contents = 'Thank you for calling!\n';
+    const contents = '<h1>Thank you for calling!</h1>';
     res.writeHead(200, {
         'Content-Length': contents.length,
         'Content-Type': 'text/plain'
